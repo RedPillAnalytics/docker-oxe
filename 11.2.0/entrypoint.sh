cdb0524318d71f147eac2158884585efe7954c13
Stewart Bryson <stewart@redpillanalytics.com>
2019-12-28 12:45:49 -0500

    Removed 11.2.0 directory.

--- 11.2.0/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$ORACLE_BASE/runOracle.sh
-
-# echo "The following output is now a tail of the alert.log:"
-tail -f $ORACLE_BASE/diag/rdbms/*/*/trace/alert*.log &
-childPID=$!
-wait $childPID
